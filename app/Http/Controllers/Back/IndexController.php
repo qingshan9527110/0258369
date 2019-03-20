a0c9253f25904223fa4ad8bbbc68f09fc350d052
qingshan9527110 <qingshan9527110@qq.com>
2019-03-20 11:32:58 +0800

    3

---
+++ app/Http/Controllers/Back/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/3/19 0019
+ * Time: 17:07
+ */
+
+namespace App\Http\Controllers\Back;
+
+
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        return 'index';
+    }
+}
